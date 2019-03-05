123b5c0910e000cf9b00a37146aae99a835f3063
Patrick Venture <venture@google.com>
2019-03-05 14:01:00 -0800

    initial commit
    Add initial code from phosphor-ipmi-flash/tools that was not specific to
    firmware update over ipmi-blobs.
    
    Change-Id: I360537a7392347fe989397a699f6a712bc36e62c
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ src/ipmiblob/test/ipmi_interface_mock.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <ipmiblob/ipmi_interface.hpp>
+
+#include <gmock/gmock.h>
+
+namespace host_tool
+{
+
+class IpmiInterfaceMock : public IpmiInterface
+{
+  public:
+    virtual ~IpmiInterfaceMock() = default;
+    MOCK_METHOD1(sendPacket,
+                 std::vector<std::uint8_t>(std::vector<std::uint8_t>&));
+};
+
+} // namespace host_tool
