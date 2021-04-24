8da5f72db5a9d3374570cb860f52f35d518ac1c2
Willy Tu <wltu@google.com>
2021-04-24 16:30:50 -0700

    test: Replace the C++ MOCK_METHOD<n> macros with the new MOCK_METHOD
    Change-Id: Iebd23cce5dbb2ac047c9df1db2320b0402cd2499
    Signed-off-by: Willy Tu <wltu@google.com>

--- src/ipmiblob/test/ipmi_interface_mock.hpp
+++ src/ipmiblob/test/ipmi_interface_mock.hpp
@@ -11,9 +11,9 @@ class IpmiInterfaceMock : public IpmiInterface
 {
   public:
     virtual ~IpmiInterfaceMock() = default;
-    MOCK_METHOD3(sendPacket,
-                 std::vector<std::uint8_t>(std::uint8_t, std::uint8_t,
-                                           std::vector<std::uint8_t>&));
+    MOCK_METHOD(std::vector<std::uint8_t>, sendPacket,
+                (std::uint8_t, std::uint8_t, std::vector<std::uint8_t>&),
+                (override));
 };
 
 } // namespace ipmiblob
